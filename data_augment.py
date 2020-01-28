c0222e9e0e4a722f701ea055288451b8d4981765
kodamanbou <kodamanbou0424@gmail.com>
2020-01-29 02:03:28 +0900

    Add data augmentation.

--- data_augment.py
+++ data_augment.py
@@ -3,4 +3,18 @@ import numpy as np
 
 
 def time_masking(wav, t):
+    num_frames = len(wav)
+    assert t <= num_frames
+    start = np.random.randint(0, num_frames - t)
+    end = start + t
+    wav[start:end] = np.min(wav)
+
+    return wav
+
+
+def frequency_masking(wav):
+    print()
+
+
+def time_warping(wav):
     print()
