2d7356a8eb28947b4bfe2129f691ca1d96ce86e6
kodamanbou <kodamanbou0424@gmail.com>
2020-02-14 20:07:03 +0900

    Add learning rate decay.

--- hyperparameter.py
+++ hyperparameter.py
@@ -1,17 +1,18 @@
-logdir = 'logdir'
-weights_dir = 'weights'
-data_dir = 'datasets'
-rate = 22050
-batch_size = 1
-generator_lr = 0.0002
-discriminator_lr = 0.0001
-lambda_cycle = 10
-lambda_identity = 5
-num_mceps = 36
-duration = 5.0
-n_frames = 128
-output_size = 512
-num_iterations = 2400000
+class Hyperparameter:
+    logdir = 'logdir'
+    weights_dir = 'weights'
+    data_dir = 'datasets'
+    rate = 22050
+    batch_size = 1
+    generator_lr = 0.0002
+    discriminator_lr = 0.0001
+    lambda_cycle = 10
+    lambda_identity = 5
+    num_mceps = 36
+    duration = 5.0
+    n_frames = 128
+    output_size = 512
+    num_iterations = 2400000
 
-# data augmentation.
-n_mask_frames = 8
+    # data augmentation.
+    n_mask_frames = 8
